752e2c43b8d39b83f1f67e1c748ccfc686b0bb88
SouravJ7860 <souravjangra0070@gmail.com>
2026-07-26 13:24:05 +0530

    Fix frontend dependencies

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,15 +1,20 @@
 // import axios from "axios";
 
 // const api = axios.create({
-//   baseURL: "http://localhost:5000/api",
+//     baseURL: "http://localhost:5000/api",
 // });
+
+// api.interceptors.response.use(
+//     (response) => response.data,
+//     (error) => Promise.reject(error.response?.data || error)
+// );
 
 // export default api;
 
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: `${import.meta.env.VITE_API_URL}/api`,
 });
 
 api.interceptors.response.use(
--- frontend/src/socket/socket.js
+++ frontend/src/socket/socket.js
@@ -1,7 +1,15 @@
+// import { io } from "socket.io-client";
+
+// const socket = io("http://localhost:5000", {
+//   transports: ["websocket"],
+// });
+
+// export default socket;
+
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
-  transports: ["websocket"],
+const socket = io(import.meta.env.VITE_API_URL, {
+    transports: ["websocket"],
 });
 
 export default socket;
