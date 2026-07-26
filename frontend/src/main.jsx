6aacce884c77e98cf18887851fb2cb30fa7fb4ff
SouravJ7860 <souravjangra0070@gmail.com>
2026-07-26 13:48:10 +0530

    Fix App component filename case

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,7 @@ import "react-toastify/dist/ReactToastify.css";
 
 import "./styles/global.css";
 
-import App from "./App";
+import App from "./app";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
